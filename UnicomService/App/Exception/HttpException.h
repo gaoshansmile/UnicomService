bd0c834de0b5015d670dc4c3b3fbf9ac54ef309c
老邢 Thierry <Enway@EnwaytekiMacBook-Pro.local>
2013-01-15 19:17:23 +0800

    Revert "a"
    This reverts commit c0931a8b42cf378957e6cad039c76f247139c321.

---
+++ UnicomService/App/Exception/HttpException.h
@@ -0,0 +1,13 @@
+//
+//  HttpException.h
+//  UnicomService
+//
+//  Created by 老邢 Thierry on 13-1-15.
+//  Copyright (c) 2013年 Enway. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpException : NSException
+
+@end
---
+++ UnicomService/App/Exception/HttpException.m
@@ -0,0 +1,13 @@
+//
+//  HttpException.m
+//  UnicomService
+//
+//  Created by 老邢 Thierry on 13-1-15.
+//  Copyright (c) 2013年 Enway. All rights reserved.
+//
+
+#import "HttpException.h"
+
+@implementation HttpException
+
+@end
